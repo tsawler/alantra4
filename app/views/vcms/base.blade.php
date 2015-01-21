70dfd8083b3d6ba60aedf22d506d035863d9c242
Trevor Sawler <trevor.sawler@me.com>
2015-01-21 15:35:10 -0400

    Product page (public) in progress

--- app/views/vcms/base.blade.php
+++ app/views/vcms/base.blade.php
@@ -64,7 +64,7 @@
 
             <header>
 
-                <ul class="breadcrumb"><!-- breadcrumb -->
+                <ul class="breadcrumb pull-right"><!-- breadcrumb -->
                     <li><a href="/">{{ Lang::get('vcms::menu.home') }}</a> </li>
                     @yield('breadcrumb')
                 </ul><!-- /breadcrumb -->
