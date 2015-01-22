111fb6de72171f59714177225cf17af78759c42d
Trevor Sawler <trevor.sawler@me.com>
2015-01-22 14:06:55 -0400

    roles

--- app/controllers/AlantraPageController.php
+++ app/controllers/AlantraPageController.php
@@ -10,6 +10,8 @@ class AlantraPageController extends \verilion\vcms\PageController {
     public function postEditpage()
     {
         $page_id = Input::get('page_id');
+        $file = Input::file('image_name');
+
         if ($page_id > 0)
         {
             $page = verilion\vcms\Page::find($page_id);
@@ -31,8 +33,59 @@ class AlantraPageController extends \verilion\vcms\PageController {
             $page->page_content_fr = Input::get('page_content_fr');
         }
 
+        $id = $page->save();
+
+        // handle image, if any
+        if (Input::hasFile('image_name'))
+        {
+            $destinationPath = base_path() . '/public/page_images/';
+            $filename = $file->getClientOriginalName();
+            $upload_success = Input::file('image_name')->move($destinationPath, $filename);
+            if (!File::exists($destinationPath . "thumbs"))
+            {
+                File::makeDirectory($destinationPath . "thumbs");
+            }
+            $thumb_img = Image::make($destinationPath . $filename);
+            $height = $thumb_img->height();
+            $width = $thumb_img->width();
+
+            if (($height < 532) || ($width < 1600))
+            {
+                File::delete($destinationPath . $filename);
+                return Redirect::to('/admin/pages/page?id=' . $id)
+                    ->with('error', 'Your image is too small. It must be at least '
+                        . '1600 '
+                        . ' pixels wide, and '
+                        . '532 '
+                        . ' pixels tall!');
+                exit;
+            }
+
+            $thumb_img->fit(Config::get('vcms::thumb_size'), Config::get('vcms::thumb_size'))
+                ->save($destinationPath . "thumbs/" . $filename);
+
+            unset($thumb_img);
+            $img = Image::make($destinationPath . $filename);
+
+            $width = $img->width();
+            if (($width > 1600) || ($height > 532))
+            {
+                // this image is very large; we'll need to resize it.
+                $img = $img->fit(1600, 532);
+                $img->save();
+            }
+
+            if ($upload_success)
+            {
+                $item = new PageImage;
+                $item->page_id = $id;
+                $item->image_name = $filename;
+                $item->save();
+            }
+
+        }
+
         Cache::flush();
-        $page->save();
 
         return Redirect::to('/admin/page/all-pages')->with('message', 'Page saved successfully');
     }
