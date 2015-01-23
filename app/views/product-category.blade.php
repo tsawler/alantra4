7643abf7ac0b0a90b499769921318455bbbe307b
Trevor Sawler <trevor.sawler@me.com>
2015-01-23 13:17:01 -0400

    layout

--- app/views/product-category.blade.php
+++ app/views/product-category.blade.php
@@ -30,44 +30,40 @@
     @endif
 
     @foreach($products as $product)
-
         <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
+            <div class="col-md-1"></div>
             <?php
             $item = Product::find($product->id);
             ?>
-            <div class="row">
-                <div class="col-md-2">
-                    <?php
-                        $images = $item->images;
-                        if (sizeof($images) > 0){
-                            ?>
-                            <a href="/products/product/{{ $item->slug }}">
-                                <img class='img img-responsive img-thumbnail' src="/product_images/thumbs/{{ $images[0]->image_name }}">
-                            </a>
-                            <?php
-                        } else {
-                            ?>
-                            <a href="/products/product/{{ $item->slug }}">
-                                <img class='img img-responsive img-thumbnail' src="http://placehold.it/350x350&text=No+Image">
-                            </a>
-                            <?php
-                        }
-                    ?>
-                </div>
-                <div class="col-md-10">
-                    @if ((Session::has('lang')) && (Session::get('lang') == 'fr'))
-                        <h4><a href="/products/product/{{ $item->slug }}">{{ $item->title_fr }}</a></h4>
-                        {{ $item->description_fr }}
-                    @else
-                        <h4><a href="/products/product/{{ $item->slug }}">{{ $item->title }}</a></h4>
-                        {{ $item->description }}
-                    @endif
-                </div>
+            <div class="col-md-2">
+                <?php
+                    $images = $item->images;
+                    if (sizeof($images) > 0){
+                        ?>
+                        <a href="/products/product/{{ $item->slug }}">
+                            <img class='img img-responsive thumbnail' src="/product_images/thumbs/{{ $images[0]->image_name }}">
+                        </a>
+                        <?php
+                    } else {
+                        ?>
+                        <a href="/products/product/{{ $item->slug }}">
+                            <img class='img img-responsive thumbnail' src="http://placehold.it/350x350&text=No+Image">
+                        </a>
+                        <?php
+                    }
+                ?>
             </div>
-        </div>
-        <div class="col-md-2"></div>
+            <div class="col-md-8">
+                @if ((Session::has('lang')) && (Session::get('lang') == 'fr'))
+                    <h4><a href="/products/product/{{ $item->slug }}">{{ $item->title_fr }}</a></h4>
+                    {{ $item->description_fr }}
+                @else
+                    <h4><a href="/products/product/{{ $item->slug }}">{{ $item->title }}</a></h4>
+                    {{ $item->description }}
+                @endif
+            </div>
+
+            <div class="col-md-1"></div>
         </div>
     @endforeach
 @stop
