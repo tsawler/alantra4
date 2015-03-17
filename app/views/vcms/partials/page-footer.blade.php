0c395a2db03c993f9a640185cd674f40107a8a5e
Trevor Sawler <trevor.sawler@me.com>
2015-03-16 23:40:30 -0300

    subscriber

--- app/views/vcms/partials/page-footer.blade.php
+++ app/views/vcms/partials/page-footer.blade.php
@@ -61,9 +61,9 @@
                 <h4>Subscribe to our <strong>Newsletter</strong></h4>
                 <form id="newsletterSubscribe" method="post" action="/subscribe" class="input-group">
                     <input required type="email" class="form-control" name="newsletter_email" id="newsletter_email" value="" placeholder="E-mail Address">
-								<span class="input-group-btn">
-									<button class="btn btn-primary">SUBMIT</button>
-								</span>
+                        <span class="input-group-btn">
+                            <button class="btn btn-primary">SUBMITT</button>
+                        </span>
                 </form>
 
             </div>
