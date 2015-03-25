93edc3749172fd8c538071396c9fd0619583d97f
Trevor Sawler <trevor.sawler@gmail.com>
2015-03-24 22:42:55 -0300

    language button

--- app/views/packages/verilion/vcms/partials/language-menu.blade.php
+++ app/views/packages/verilion/vcms/partials/language-menu.blade.php
@@ -1,6 +1,6 @@
 @if (Config::get('vcms::use_french'))
     <span class="pull-right" style="padding: 2px; padding-top: 11px;">
-        &nbsp;&nbsp;<a href="{{ Lang::get('vcms::menu.lang') }}&amp;url={{ URL::current() }}" style="color: black;">
+        &nbsp;&nbsp;<a class="btn btn-xs btn-primary" style="color: white" href="{{ Lang::get('vcms::menu.lang') }}&amp;url={{ URL::current() }}" style="color: black;">
         {{ Lang::get('vcms::menu.language_switch') }}
         </a>
     </span>
