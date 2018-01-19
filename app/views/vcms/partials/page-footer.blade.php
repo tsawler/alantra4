ef470d4cf53a9fa6614bd2db9269a180faf40001
Trevor Sawler <trevor.sawler@gmail.com>
2018-01-19 14:53:28 -0400

    Google tag manager

--- app/views/vcms/partials/page-footer.blade.php
+++ app/views/vcms/partials/page-footer.blade.php
@@ -160,25 +160,7 @@
     </div>
 </noscript>
 @yield('bottom-js')
-<!-- Google Code for Remarketing Tag -->
-<!--------------------------------------------------
-Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. See more information and instructions on how to setup the tag on: http://google.com/ads/remarketingsetup
---------------------------------------------------->
-<script type="text/javascript">
-    var google_tag_params = {
-        dynx_itemid: 'REPLACE_WITH_VALUE',
-        dynx_itemid2: 'REPLACE_WITH_VALUE',
-        dynx_pagetype: 'REPLACE_WITH_VALUE',
-        dynx_totalvalue: 'REPLACE_WITH_VALUE',
-    };
-</script>
-<script type="text/javascript">
-    /* <![CDATA[ */
-    var google_conversion_id = 1039606680;
-    var google_custom_params = window.google_tag_params;
-    var google_remarketing_only = true;
-    /* ]]> */
-</script>
+
 <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
 </script>
 <noscript>
